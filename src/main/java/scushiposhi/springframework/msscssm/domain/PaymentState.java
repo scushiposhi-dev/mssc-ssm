43718cd41a99f46dd63de4a3ba527ffdb30df8c5
scushiposhi-dev <scushiposhi@yahoo.it>
2019-12-07 16:50:33 +0100

    adding states and events enumerations

---
+++ src/main/java/scushiposhi/springframework/msscssm/domain/PaymentState.java
@@ -0,0 +1,9 @@
+package scushiposhi.springframework.msscssm.domain;
+
+/**
+ * @author scushiposhi on 07/12/2019
+ * @project IntelliJ IDEA
+ */
+public enum PaymentState {
+   NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR
+}
